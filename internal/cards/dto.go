31c81647abb172626a93629759c4be143c500654
operaodev <natanaelmaguinosoto@gmail.com>
2026-05-07 13:27:18 -0500

    definiendo estructura para busqueda y resumen

--- internal/cards/dto.go
+++ internal/cards/dto.go
@@ -1,5 +1,6 @@
 package cards
 
+// Cartas recomendadas en el buscador
 type RecomendationCardDTO struct {
 	ID    uint64 `json:"id"`
 	Name  string `json:"name"`
@@ -7,15 +8,24 @@ type RecomendationCardDTO struct {
 	Image string `json:"image"`
 }
 
+// Impresiones de cartas recomendadas en el buscador
 type RecomendationPrintedCardDTO struct {
-	ID             uint64   `json:"id"`
-	TCG            TCG      `json:"tcg"`
-	Name           string   `json:"name"`
-	EnglishName    string   `json:"englishName"`
-	Code           string   `json:"code"`
-	Rarity         Rarity   `json:"rarity"`
-	RarityCode     string   `json:"rarityCode"`
-	SetName        string   `json:"setName"`
-	Image          string   `json:"image"`
-	Print          Print    `json:"print"`
+	ID          uint64 `json:"id"`
+	TCG         TCG    `json:"tcg"`
+	Name        string `json:"name"`
+	EnglishName string `json:"englishName"`
+	Code        string `json:"code"`
+	Rarity      Rarity `json:"rarity"`
+	RarityCode  string `json:"rarityCode"`
+	SetName     string `json:"setName"`
+	Image       string `json:"image"`
+	Print       Print  `json:"print"`
+}
+
+type SummaryPrintedCardDTO struct {
+	RecomendationCardDTO
+	TotalForSale uint    `json:"onSale"`
+	MinPrice     float64 `json:"minPrice"`
+	AvgPrice     float64 `json:"avgPrice"`
+	MaxPrice     float64 `json:"maxPrice"`
 }
